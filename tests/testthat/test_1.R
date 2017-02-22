7f2b0ea1ef5da6013dc7983d36a198da0fcf1ee8
Rene Plonus <r.plonus@web.de>
2017-02-22 16:51:42 +0100

    .

--- tests/testthat/test_1.R
+++ tests/testthat/test_1.R
@@ -1,7 +1,8 @@
 context("Calculations correct")
 
-testthat::test_that("calculate correct age/ hc/ otoradius", {
-  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[[1]][1, "age"], test$V5[1])
-  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[[1]][1, "hc"], test$V3[1])
-  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[[1]][1, "oto_rad"], test$V4[1])
+testthat::test_that("calculate mastertable correct ", {
+  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[1, "age"], test$V5[1])
+  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[1, "hc"], test$V3[1])
+  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[, "ring_width"], test$V2)
+  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[1, "oto_rad"], test$V4[1])
 })
