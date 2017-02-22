b8936163333bdea5bdfe239fefe4e9def70cdc22
Rene Plonus <r.plonus@web.de>
2017-02-22 16:51:33 +0100

    moved test2 into test1

--- tests/testthat/test_2.R
+++
@@ -1,5 +0,0 @@
-context("Calculations correct")
-
-testthat::test_that("calculate correct ring width", {
-  testthat::expect_equal(oto_makro(x = example[,3], y = example[,4], fish_no = 41)[[1]][, "ring_width"], test$V2)
-})
