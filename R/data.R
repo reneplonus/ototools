ddd8ebb4dd07133f78121b0aa89ce838966e4796
Rene Plonus <r.plonus@web.de>
2017-02-23 12:07:51 +0100

    moved dummy_data to test_data and renamed test_data to data

--- R/data.R
+++ R/data.R
@@ -1,3 +1,12 @@
+#' test data
+
+#' @format Dummy data:
+#' \describe{
+#' \item{x pos}{x position}
+#' \item{y pos}{y position}
+#' }
+"test"
+
 #' dummy data
 
 #' @format Dummy data:
--- R/test_data.R
+++
@@ -1,8 +0,0 @@
-#' test data
-
-#' @format Dummy data:
-#' \describe{
-#' \item{x pos}{x position}
-#' \item{y pos}{y position}
-#' }
-"test"
