6dbeeba5c9d602b90023a64c406e9ee7d4c9c48c
Rene Plonus <r.plonus@web.de>
2017-05-26 11:48:20 +0200

    apply tibble struc

--- R/comb_func.R
+++ R/comb_func.R
@@ -13,15 +13,18 @@
 #' @examples
 #' #rm(list = ls())
 #' x <- oto_makro(x = example[,3], y = example[,4], fish_no = "example")
-#' x$id <- as.character(x$id)
 #' y <- gathering
-#' combi <- comb_func(x = x, y = y)
+#' y$age <- as.numeric(y$age)
+#' combi <- comb_func(x = x, y = y, by = c("id", "age"))
 #'
 
 comb_func <- function(x, y, by = "id") {
-  if (class(x[, by]) != class(y[, by])) stop(paste0("Column types not identical in x and y.",
-                                                    by, " in x is ", class(x[, by]),
-                                                    " while ", by, " in y is ", class(y[, by])))
+  for(i in 1:length(by)) {
+    if (class(x[, by[i]][[1]]) != class(y[, by[i]])) stop(paste0("Column types not identical in x and y. ",
+                                                           by[i], " in x is ", class(x[, by[i]][[1]]),
+                                                           " while ", by[i], " in y is ", class(y[, by[i]])))
+  }
+
   z <- dplyr::left_join(x, y, by = by)
   return(z)
 }
