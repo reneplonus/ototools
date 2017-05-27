80aa3bb0e1c89d20ab4f50d71a92e93b1b3280f3
Rene Plonus <r.plonus@web.de>
2017-05-27 12:20:33 +0200

    ...

--- R/master_func.R
+++ R/master_func.R
@@ -43,10 +43,10 @@ master_func <- function(fish, format = "txt", sep = "\t", basic = NULL, catch_da
     if(!is.numeric(x[,catch_day][[1]])) {
       if(!is.integer(x[,catch_day][[1]])) stop("Catch_day has to be either of type numeric or integer")
     }
-    x$julday <- purrr::map2_dbl(.x = x$catch_date, .y = x$age, .f = ~.x - .y)
+    x$julday <- purrr::map2_dbl(.x = unlist(x[, catch_day]), .y = x$age, .f = ~.x - .y)
     x$julday <- purrr::map2(.x = x$ring_no, .y = x$julday, .f = ~.x + .y + 1)
-    x$move_ave <- purrr::map2(.x = x$ring_no, .y = x$ring_width, .f = ~move_ave(.x, .y))
-    }
+  }
+  x$move_ave <- purrr::map2(.x = x$ring_no, .y = x$ring_width, .f = ~move_ave(.x, .y))
   return(x)
 }
 
