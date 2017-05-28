d0102d550f0290599abe9bb5744eb2e3bd58024a
Rene Plonus <r.plonus@web.de>
2017-05-28 13:37:06 +0200

    ...

--- tests/testthat/test_move_ave.R
+++ tests/testthat/test_move_ave.R
@@ -2,7 +2,7 @@ context("Calculations correct")
 dat <- move_ave(master_tib$ring_no[[1]], master_tib$ring_width[[1]])
 
 testthat::test_that("calculate mastertable correct ", {
-  testthat::expect_equal(dat[1:4], c(NA, NA, NA, NA))
-  testthat::expect_equal(dat[(length(dat) - 3) : length(dat)], c(NA, NA, NA, NA))
+  testthat::expect_equal(dat[1:4], as.numeric(c(NA, NA, NA, NA)))
+  testthat::expect_equal(dat[(length(dat) - 3) : length(dat)], as.numeric(c(NA, NA, NA, NA)))
   testthat::expect_equal(dat[36], master_tib$move_ave[[1]][36])
 })
