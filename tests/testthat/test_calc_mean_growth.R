29dc102861526abe2f3429ecbd42263ecd5266d6
Rene Plonus <r.plonus@web.de>
2017-05-29 12:28:43 +0200

    ...

--- tests/testthat/test_calc_mean_growth.R
+++ tests/testthat/test_calc_mean_growth.R
@@ -3,7 +3,7 @@ context("Load in data correct")
 dat <- calc_mean_growth(master_tib$julday, master_tib$ring_width)
 
 testthat::test_that("calculate mastertable correct ", {
-  testthat::expect_equal(dat$se_growth[1], 0)
+  testthat::expect_equal(dat$sd_growth[1], 0)
   testthat::expect_equal(dat$mean_growth[5], master_tib$ring_width[[1]][5])
   testthat::expect_equal(dat$julday[1], min(unlist(master_tib$julday)))
   testthat::expect_equal(dat$julday, unique(unlist(master_tib$julday)))
