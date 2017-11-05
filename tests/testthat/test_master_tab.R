f3396fe4fd14887b582c4e41073cc5055c439a77
Rene Plonus <r.plonus@web.de>
2017-11-05 13:24:32 +0100

    update test master_tab

--- tests/testthat/test_master_tab.R
+++ tests/testthat/test_master_tab.R
@@ -1,7 +1,8 @@
 context("Calculations correct")
 dat <- master_tab(list(x = makro_output, x = makro_output))
 
-testthat::test_that("calculate mastertable correct ", {
-  testthat::expect_equal(nrow(dat), nrow(makro_output) * 2)
-  testthat::expect_equal(dat$id[[1]], dat$id[[2]])
+test_that("calculate mastertable correct ", {
+  expect_equal(nrow(dat), nrow(makro_output) * 2)
+  expect_equal(dat$id[[1]], dat$id[[2]])
+  expect_error(master_tab(list(makro_output, makro_output[,1:(ncol(makro_output) - 1)])))
 })
