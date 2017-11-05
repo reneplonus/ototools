536b31979fa939da4c7a44e9a410726500dfee21
Rene Plonus <r.plonus@web.de>
2017-11-05 12:28:37 +0100

    update error - wasnt working properly..

--- R/master_tab.R
+++ R/master_tab.R
@@ -15,7 +15,7 @@
 #'
 
 master_tab <- function(x) {
-  if(any(ncol(x) != ncol(x[[1]]))) stop("All dfrs must have as much columns as the first one.")
+  if(any(purrr::map_lgl(x, ~ncol(.) != ncol(x[[1]])))) stop("All dfrs must have as much columns as the first one.")
   temp <- x[[1]]
   for(i in 2:length(x)) {
     temp <- rbind(temp, x[[i]], deparse.level = 0)
