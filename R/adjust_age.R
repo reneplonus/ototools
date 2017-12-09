9d224cf8f27c5eab8b33a56f317b4ef692694a92
Rene Plonus <r.plonus@web.de>
2017-12-09 14:42:13 +0100

    add adjust_age

---
+++ R/adjust_age.R
@@ -0,0 +1,71 @@
+#' adjust_age
+#'
+#' Adjusts the number of rings.
+#'
+#' If the counting of rings does not start at the hatch check, this function adjusts the values given by
+#' \link{oto_makro}.
+#'
+#' @param x an output from \link{oto_makro} to be corrected
+#' @param value a vector with values tp be added to ring_no
+#' @param mean a vector with values to be matched with hc
+#' @param sd a vector indicating the possible error of the values given in mean
+#' @param data instead of passing value, mean and sd you can simply pass a dfr including all three values.
+#' In that case, names is needed.
+#' @param names a character vector of length 3 indicating which column in data represents which argument
+#' needed in adjust_rings (value, mean, sd)
+#' @export
+#'
+#' @examples
+#' #rm(list = ls())
+#' x <- makro_output
+#' data <- data.frame(ring_no = c(1:10),
+#'                    mean = sort(seq(from = 5, to = 15, length.out = 10) + runif(10, -1, 1)),
+#'                    sd = runif(10, 0, 2))
+#' names <- c("ring_no", "mean", "sd")
+#' #using data:
+#' adjust_age(x, data = data, names = names)
+#' #without using data:
+#' adjust_age(x, data$ring_no, data$mean, data$sd)
+#'
+
+adjust_age <- function(x, value = NULL, mean = NULL, sd = NULL, data = NULL, names = NULL) {
+
+  #check input
+  if(!is.null(data)) {
+    value <- data[,names[1]]
+    mean <- data[,names[2]]
+    sd <- data[,names[3]]
+  }
+
+  #get false HC
+  hc <- x$hc
+
+  #get sd for value
+  mean_plus <- mean + sd
+  mean_minus <- mean - sd
+
+  diff_plus <- NULL
+  diff_minus <- NULL
+
+  for(i in seq_along(mean_plus)) {
+    diff_plus[i] <- value[max(which(mean < mean_plus[i]))] - value[i]
+    diff_minus[i] <- value[i] - value[suppressWarnings(max(which(mean < mean_minus[i])))]
+  }
+  sd_value <- purrr::map2_dbl(diff_plus, diff_minus, ~round(mean(c(.y, .x), na.rm = TRUE), digits = 0))
+
+  #match hc with mean
+  matching_value <- max(which(mean < hc))
+
+  #update x
+  x$hc <- as.numeric(NA)
+  x$ring_no <- purrr::map2(x$ring_no, list(value[matching_value]), ~.x + .y)
+  x$age <- x$age + value[matching_value]
+
+  #warning, if sd_value for matching_value is greater than 5% of age
+  if(sd_value[matching_value] > (.05 * x$age)) {
+    warning(paste0("The possible error for ", x$id, " exceeds 5% of the respective age!"))
+  }
+
+  #return x
+  return(x)
+}
---
+++ tests/testthat/test_adjust_age.R
@@ -0,0 +1,40 @@
+context("Calculations correct")
+x <- makro_output
+#generated randomly once
+# a <- sort(seq(from = 5, to = 15, length.out = 10) + runif(10, -1, 1))
+# b <- runif(10, 0, 2)
+a <- c(4.787283, 6.447875, 7.637744, 8.633590, 9.826411 10.253712, 11.032269, 12.331923, 13.345387, 14.761222)
+b <- c(1.9648557, 1.1850090, 1.7615487, 1.1492736, 1.3776724, 0.2618423, 0.6018208, 0.6164486, 0.3997028, 0.7165491)
+
+data <- data.frame(ring_no = c(1:10),
+                   mean = a,
+                   sd = b)
+names <- c("ring_no", "mean", "sd")
+#using data:
+test1 <- adjust_age(x, data = data, names = names)
+#without using data:
+test2 <- adjust_age(x, data$ring_no, data$mean, data$sd)
+
+#test warning
+data2 <- data
+data2$ring_no <- round(seq(from = 50, to = 150, length.out = 10), digits = 0)
+data2$sd <- 2
+
+#manually:
+hc <- x$hc                                                                        #hc
+match <- max(which(a < hc))                                                       #position of greatest mean < hc
+a_match <- a[match]                                                               #has to be < hc
+a_match2 <- a[match + 1]                                                          #has to be > hc
+sd_value <- mean(c(data$ring_no[max(which(a < (a[match] + b[match])))] - match,   #mean + sd
+                   match - data$ring_no[max(which(a < (a[match] - b[match])))]))  #mean - sd
+sd_value <- round(sd_value, digits = 0)
+new_age <- x$age + data$ring_no[match]
+
+test_that("test adjust_age", {
+  expect_equal(test1$age, new_age)
+  expect_equal(test2$hc, as.numeric(NA))
+  expect_equal(x$ring_no[[1]] + match, test1$ring_no[[1]])
+  expect_true(a_match < hc)
+  expect_true(a_match2 > hc)
+  expect_warning(adjust_age(x = x, data = data2, names = names))
+})
