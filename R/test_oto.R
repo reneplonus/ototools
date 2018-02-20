178e390765d4850a3ca2f7472bf48a08f9705161
Rene Plonus <r.plonus@web.de>
2018-02-20 21:07:18 +0100

    add param to make plot optional in test_oto

--- R/test_oto.R
+++ R/test_oto.R
@@ -9,6 +9,8 @@
 #' @param sep the separator used by the file to be loaded; default is tab
 #' @param dir working directory
 #' @inheritParams load_fish2
+#' @param add.plot Logical. If TRUE (default), a plot with ring width VS ring number will be
+#' created additionally.
 #' @export
 #'
 #' @examples
@@ -18,7 +20,7 @@
 #' test_oto(fish = fish, format = format, sep = ";", dir = substr(getwd(), 1, 11))
 #'
 
-test_oto <- function(fish, format = "txt", sep = "\t", dir = getwd(), section = NULL) {
+test_oto <- function(fish, format = "txt", sep = "\t", dir = getwd(), section = NULL, add.plot = TRUE) {
   #load in one fish and run the makro
   if(is.null(section)) {
     temp <- load_fish(fish = fish, format = format, sep = sep, dir = dir)
@@ -26,6 +28,10 @@ test_oto <- function(fish, format = "txt", sep = "\t", dir = getwd(), section = 
     temp <- load_fish2(fish = fish, sep = sep, dir = dir, section = section)
   }
   x <- oto_makro(x_coord = temp$V3, y_coord = temp$V4, fish_no = fish)
-  print(plot_growth(x_var = x$ring_no[[1]], y_var = x$ring_width[[1]]))
+  if(x$age > 0) {
+    if(add.plot) {
+      print(plot_growth(x_var = x$ring_no[[1]], y_var = x$ring_width[[1]]))
+    }
+  }
   return(x)
 }
